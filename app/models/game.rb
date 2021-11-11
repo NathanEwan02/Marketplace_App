74a2c09ecfeebdb218724261a50ee7afaf1f8dc5
Nathan <nathan.ewan14@gmail.com>
2021-11-11 13:12:18 +1000

    Creating events and team_events models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
     has_many :team_games
     has_many :teams, through: :team_games, dependent: :destroy
+    has_many :user_games
+    has_many :users, through: :user_games
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,6 @@ class Team < ApplicationRecord
     has_many :users, dependent: :nullify
     has_many :team_games
     has_many :games, through: :team_games, dependent: :destroy
+    has_many :team_events
+    has_many :events, through: :team_events
 end
