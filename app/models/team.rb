728ac7fc471a1a04329afc964c3b09869362c693
Nathan <nathan.ewan14@gmail.com>
2021-11-11 11:20:58 +1000

    Adding relationships and dependancies

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
-    has_many :users
+    has_many :users, dependent: :nullify
+    has_many :team_games
+    has_many :games, through: :team_games, dependent: :destroy
 end
