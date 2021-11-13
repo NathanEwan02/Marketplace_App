b74aa974290e1bfabdf08a2a9977f50c1f02857c
Nathan <nathan.ewan14@gmail.com>
2021-11-13 19:44:54 +1000

    Added resorces :events route to allow access to all event routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations'}
+  devise_for :users #controllers: { registrations: 'users/registrations'}
   resources :games
+  resources :events
   root to: "events#index"
 end
