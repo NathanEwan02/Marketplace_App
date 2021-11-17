c9fab1e4ed6557387c5044d72422f987477a5b49
Nathan <nathan.ewan14@gmail.com>
2021-11-17 13:08:57 +1000

    Fixed validation issues with users

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,20 +6,5 @@ class Team < ApplicationRecord
     has_many :events, through: :team_events
 
     validates :name, presence: {message: "Please provide a name"}
-    validate :user_not_taken
-    #validate :check
 
-    def check
-        u = users.empty?
-        if u == false
-            errors.add(:user_id, " is already in a team")
-        end
-    end
-
-    def user_not_taken
-        user_has_team = users.map { |u| u.team_id }.nil? 
-        if user_has_team == true
-            errors.add(:user_id, " is already in a team")
-        end
-    end
 end
