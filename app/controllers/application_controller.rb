3ff67cf62746c490d15b9b2907573bdd09fa8fa1
Nathan <nathan.ewan14@gmail.com>
2021-11-17 13:44:21 +1000

    Added username to user params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, role_ids: []])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, role_ids: []])
     end
 end
